4ebdf82b988690a1983c7658a78be7ac3e5dc9de
SagarSSuryavanshi <92152760+SagarSSuryavanshi@users.noreply.github.com>
2021-12-07 11:29:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Jenkins integration with Github and Docker");
     response.end("Hello World!");
     response.end("Jenkins integration with Github and Docker");
 });
